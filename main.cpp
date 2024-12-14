c5cab31d7ac89f6da6f34cdcec404bbeabe98a53
Khushi Yadav <kky957242@gmail.com>
2024-12-14 22:35:51 +0530

    Create main.cpp
    C++ intro class

---
+++ main.cpp
@@ -0,0 +1,12 @@
+C++ is a popular programming language
+C++ was developed by Bjarne Stroustrup. it is a general purpose, case-sensitive, free-form programming language that supports object-oriented.
+C++ is a middle-level language, as it encapsulates both high and low level language features.
+  first program 
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World ";
+  return 0;
+}
+here, cout is used to print the value.
